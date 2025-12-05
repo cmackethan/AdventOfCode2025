c55c0b1a13c94d9491689c4f3ef4f82159cc1055
Conrad MacKethan <conradmackethan@gmail.com>
2025-12-05 17:28:18 -0500

    Solve day 1 part 2

--- AdventOfCode2025/Solutions/Day1.cs
+++ AdventOfCode2025/Solutions/Day1.cs
@@ -1,3 +1,4 @@
+using System.Reflection.Emit;
 using AdventOfCode2025.Solutions.Interfaces;
 using AdventOfCode2025.Utilities;
 
@@ -5,32 +6,39 @@ namespace AdventOfCode2025.Solutions;
 
 internal class Day1 : ISolution
 {
+    private const int _dialSize = 100;
+
     public void Part1()
     {
-        var input = InputReader.ReadAsStringArray("/Users/cmackethan/dev/aoc2025/AdventOfCode2025/Input/Day1/Part1.txt");
+        var input = InputReader.ReadAsStringArray("/Users/cmackethan/dev/aoc2025/AdventOfCode2025/Input/Day1/Input.txt");
 
         var (position, password) = (50, 0);
 
         foreach (var (direction, distance) in input.Select(x => (x[0], int.Parse(x[1..]))))
         {
+            Console.WriteLine($"Input: {direction}{distance}");
+
             if (direction == 'L')
             {
-                position -= distance % 100;
+                position = (position - distance) % _dialSize;
 
-                if (position < 0) position += 100;
+                if (position < 0) position += _dialSize;
+                
+                if (position == 0) password++;
+            }
+            else if (direction == 'R')
+            {
+                position = (position + distance) % _dialSize;
                 
                 if (position == 0) password++;
             }
             else
             {
-                position += distance % 100;
-
-                if (position > 99) position -= 100;
-                
-                if (position == 0) password++;
+                throw new InvalidOperationException();
             }
 
             Console.WriteLine($"Position: {position}");
+            Console.WriteLine();
         }
 
         Console.WriteLine($"Password: {password}");
@@ -38,6 +46,51 @@ internal class Day1 : ISolution
 
     public void Part2()
     {
-        throw new NotImplementedException();
+        var input = InputReader.ReadAsStringArray("/Users/cmackethan/dev/aoc2025/AdventOfCode2025/Input/Day1/Input.txt");
+
+        var (position, password) = (50, 0);
+
+        foreach (var (direction, distance) in input.Select(x => (x[0], int.Parse(x[1..]))))
+        {
+            Console.WriteLine($"Input: {direction}{distance}");
+
+            var rotations = 0;
+
+            if (direction == 'L')
+            {
+                var positionWasZero = position == 0;
+
+                (rotations, position) = Math.DivRem(position - distance, _dialSize);
+
+                rotations = Math.Abs(rotations);
+
+                if (position < 0)
+                {
+                    position += _dialSize;
+
+                    if (!positionWasZero && position != 0) rotations++;
+                }
+
+                if (position == 0) rotations++;
+
+                password += rotations;
+            }
+            else if (direction == 'R')
+            {
+                (rotations, position) = Math.DivRem(position + distance, _dialSize);
+
+                password += rotations;
+            }
+            else
+            {
+                throw new InvalidOperationException();
+            }
+
+            Console.WriteLine($"Rotations: {rotations}");
+            Console.WriteLine($"Position: {position}");
+            Console.WriteLine();
+        }
+
+        Console.WriteLine($"Password: {password}");
     }
 }
