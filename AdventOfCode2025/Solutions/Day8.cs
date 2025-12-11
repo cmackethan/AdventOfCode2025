3ec53132614a6304f8f98986f569c141fce71313
Conrad MacKethan <conradmackethan@gmail.com>
2025-12-10 22:19:32 -0500

    Cleanup Day 8

--- AdventOfCode2025/Solutions/Day8.cs
+++ AdventOfCode2025/Solutions/Day8.cs
@@ -1,4 +1,3 @@
-using System.Xml.Linq;
 using AdventOfCode2025.Solutions.Interfaces;
 using AdventOfCode2025.Utilities;
 
@@ -7,6 +6,7 @@ namespace AdventOfCode2025.Solutions;
 internal class Day8 : ISolution
 {
     private readonly string[] _input = InputReader.ReadAsStringArray("Day8/Input.txt");
+    private readonly List<HashSet<Position>> _circuits = [];
 
     private class Position
     {
@@ -41,43 +41,19 @@ internal class Day8 : ISolution
     {
         var positions = _input
             .Select(x => x.Split(',').Select(x => int.Parse(x)).ToArray())
-            .Select(x => new Position() { X = x[0], Y = x[1], Z = x[2] })
-            .ToArray();
+            .Select(x => new Position() { X = x[0], Y = x[1], Z = x[2] });
 
         var combinations = positions
             .SelectMany((_, i) => positions.Skip(i + 1), (pos1, pos2) => (pos1, pos2))
             .OrderBy(x => x.pos1.GetDistance(x.pos2))
             .Take(1000);
 
-        var circuits = new List<HashSet<Position>>();
         foreach (var (pos1, pos2) in combinations)
         {
-            var pos1Circuit = circuits.SingleOrDefault(x => x.Contains(pos1));
-            var pos2Circuit = circuits.SingleOrDefault(x => x.Contains(pos2));
-
-            if (pos1Circuit is not null && pos2Circuit is not null)
-            {
-                if (pos1Circuit != pos2Circuit)
-                {
-                    pos1Circuit.UnionWith(pos2Circuit);
-                    circuits.Remove(pos2Circuit);    
-                }
-            }
-            else if (pos1Circuit is not null)
-            {
-                pos1Circuit.Add(pos2);
-            }
-            else if (pos2Circuit is not null)
-            {
-                pos2Circuit.Add(pos1);
-            }
-            else
-            {
-                circuits.Add([pos1, pos2]);
-            }
+            CombineCircuits(pos1, pos2);
         }
 
-        var size = circuits
+        var size = _circuits
             .Select(x => x.Count)
             .OrderByDescending(x => x)
             .Take(3)
@@ -97,38 +73,42 @@ internal class Day8 : ISolution
             .SelectMany((_, i) => positions.Skip(i + 1), (pos1, pos2) => (pos1, pos2))
             .OrderBy(x => x.pos1.GetDistance(x.pos2));
 
-        var circuits = new List<HashSet<Position>>();
         foreach (var (pos1, pos2) in combinations)
         {
-            var pos1Circuit = circuits.SingleOrDefault(x => x.Contains(pos1));
-            var pos2Circuit = circuits.SingleOrDefault(x => x.Contains(pos2));
+            CombineCircuits(pos1, pos2);
 
-            if (pos1Circuit is not null && pos2Circuit is not null)
-            {
-                if (pos1Circuit != pos2Circuit)
-                {
-                    pos1Circuit.UnionWith(pos2Circuit);
-                    circuits.Remove(pos2Circuit);    
-                }
-            }
-            else if (pos1Circuit is not null)
-            {
-                pos1Circuit.Add(pos2);
-            }
-            else if (pos2Circuit is not null)
-            {
-                pos2Circuit.Add(pos1);
-            }
-            else
-            {
-                circuits.Add([pos1, pos2]);
-            }
-
-            if (circuits.Select(x => x.Count).Max() == positions.Length)
+            if (_circuits.Select(x => x.Count).Max() == positions.Length)
             {
                 Console.WriteLine($"Coordinates {pos1.X * pos2.X}");
                 break;
             }
+        }
+    }
+
+    private void CombineCircuits(Position pos1, Position pos2)
+    {
+        var pos1Circuit = _circuits.SingleOrDefault(x => x.Contains(pos1));
+        var pos2Circuit = _circuits.SingleOrDefault(x => x.Contains(pos2));
+
+        if (pos1Circuit is not null && pos2Circuit is not null)
+        {
+            if (pos1Circuit != pos2Circuit)
+            {
+                pos1Circuit.UnionWith(pos2Circuit);
+                _circuits.Remove(pos2Circuit);    
+            }
+        }
+        else if (pos1Circuit is not null)
+        {
+            pos1Circuit.Add(pos2);
+        }
+        else if (pos2Circuit is not null)
+        {
+            pos2Circuit.Add(pos1);
+        }
+        else
+        {
+            _circuits.Add([pos1, pos2]);
         }
     }
 }
